522810116621865b050c44b94b505661a5dfeb7e
jsplatar <30661357+jsplatar@users.noreply.github.com>
2017-08-02 10:50:13 -0500

    Movement

---
+++ elCubo-master/Assets/MoverCapsula.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCapsula : MonoBehaviour
+{
+
+    public float movementSpeed = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(Vector3.forward * Time.deltaTime* movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S)){
+            transform.Translate(Vector3.forward * Time.deltaTime * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal"), 0f));
+        }
+        if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal"), 0f));
+        }
+    }
+}
---
+++ elCubo-master/Assets/MoverPersonaje.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPersonaje : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal"), 0f));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal"), 0f));
+        }
+    }
+}
